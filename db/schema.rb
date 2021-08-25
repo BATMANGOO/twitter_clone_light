82f74245de930cddbfbdcb74271a79ad290c92b5
Prabhjot <pjsingh1359@gmail.com>
2021-08-25 14:31:52 -0700

    route tweets to proper user

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_150602) do
+ActiveRecord::Schema.define(version: 2021_08_25_211356) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
